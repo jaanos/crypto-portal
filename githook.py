6b9237959702fbfda5c6621d819fdc0e66f4a7cf
Janoš Vidali <janos.vidali@fri.uni-lj.si>
2015-06-23 00:35:59 +0200

    Trying the hook

--- githook.py
+++ githook.py
@@ -2,23 +2,30 @@ from flask import *
 import auth
 import json
 import hmac
+import ast
 import sys
+import os
 from hashlib import sha1
 
 app = Blueprint('githook', __name__)
+paths = {'refs/heads/master': '/var/crypto-portal/'
+         'refs/heads/devel': '/var/crypto-devel/'}
 
 @app.route("/githook", methods=['POST'])
 def githook():
-    #if request.headers.get('X-GitHub-Event') == "ping":
-    #    return json.dumps({'msg': 'Hi!'})
-    #if request.headers.get('X-GitHub-Event') != "push":
-    #    return json.dumps({'msg': "wrong event type"})
+    if request.headers.get('X-GitHub-Event') == "ping":
+        return json.dumps({'msg': 'Hi!'})
+    if request.headers.get('X-GitHub-Event') != "push":
+        return json.dumps({'msg': "wrong event type"})
     payload = json.loads(request.data)
     signature = request.headers.get('X-Hub-Signature').split('=')[1]
     mac = hmac.new(auth.gitkey, msg=request.data, digestmod=sha1)
     if not compare_digest(mac.hexdigest(), signature):
         abort(403)
-    return str(payload)
+    if "ref" in payload and payload["ref"] in paths:
+        path = paths[payload["ref"]]
+        os.system("%s/hooks/githook.sh %s" % (path, path))
+    return json.dumps({'msg': "OK"})
 
 # Check if python version is less than 2.7.7
 if sys.version_info < (2, 7, 7):
