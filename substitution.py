464c4356abe89927fc6003b0ec15a039b0412c48
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2015-06-23 20:53:03 +0200

    Create a template from static files

--- substitution.py
+++ substitution.py
@@ -8,4 +8,10 @@ def index():
 
 @app.route("/<difficulty>")
 def play(difficulty):
-    return "TODO"
+    if (difficulty == "hard"):
+        level = 2
+    elif (difficulty == "medium"):
+        level = 1
+    else:
+        level = 0
+    return render_template("substitution.play.html", nav = "substitution", level = level)
