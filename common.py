9aca71c8d74c7a1a0772b28baffc603b1a0d7656
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2015-06-23 17:07:02 +0200

    Fix main template

--- common.py
+++ common.py
@@ -1,4 +1,4 @@
 from flask import render_template
 
 def crypto_template(template, **context):
-    return render_template('main.html', render_template(template, **context))
+    return render_template('main.html', content = render_template(template, **context))
