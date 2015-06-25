a4aa04d7771dca8e4ebf2f4a7bb2d81785df50c2
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2015-06-26 00:43:56 +0200

    Layout for message hiding

--- steganography.py
+++ steganography.py
@@ -19,24 +19,26 @@ def colors():
 def images(idx=1):
     db = database.dbcon()
     cur = db.cursor()
+    cur.execute("SELECT id, name, DATE_FORMAT(time, '%e.%c.%Y %H:%i') AS time FROM slika ORDER BY time DESC")
+    imgs = cur.fetchall()
+    cur.execute("SELECT name FROM slika WHERE id = %s", idx)
+    r = cur.fetchone()
+    if r == None:
+        idx, name = None, None
+    else:
+        name = r[0]
     cur.close()
-    return render_template("steganography.images.html")
+    return render_template("steganography.images.html", idx=idx, name=name, imgs=imgs)
 
 @app.route("/show/<int:idx>")
 def show(idx):
     db = database.dbcon()
     cur = db.cursor()
-    cur.execute("SELECT width, height, mode, data FROM slika WHERE id = %s", idx)
+    cur.execute("SELECT data FROM slika WHERE id = %s", idx)
     r = cur.fetchone()
     cur.close()
     if r == None:
         abort(404)
-    width, height, mode, data = r
-    img = Image.frombytes(mode, (width, height), data)
-    out = StringIO()
-    img.save(out, format="PNG")
-    contents = out.getvalue()
-    out.close()
-    response = make_response(contents)
+    response = make_response(r[0])
     response.headers['Content-Type'] = 'image/png'
     return response
