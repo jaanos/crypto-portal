e10cdc728f96ae1f1b60d44ca145fd6599faa6de
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2015-06-27 20:00:01 +0200

    Enable debug mode in devel branch

--- crypto.py
+++ crypto.py
@@ -1,10 +1,11 @@
 from flask import * 
 
-import auth
+from auth import sesskey, debug
 from githook import app as githook_app
 from steganography import app as steganography_app
 
 app = Flask(__name__)
+app.debug = debug
 app.register_blueprint(githook_app)
 app.register_blueprint(steganography_app, url_prefix = '/steganography')
 
