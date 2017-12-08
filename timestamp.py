f08738cd4bb02a71c4d6d147b237ce7d6d03cc23
db1456 <db1456@student.uni-lj.si>
2017-12-08 20:22:42 +0100

    Add files via upload

--- timestamp.py
+++ timestamp.py
@@ -24,9 +24,8 @@ def decrypt(ciphertext, priv_key):
     cipher = PKCS1_OAEP.new(priv_key)
     return cipher.decrypt(b64decode(ciphertext))
 
-def sign(message, priv_key, hashAlg="SHA-256"):
+def sign(message, priv_key):
     global hash
-    hash = hashAlg
     signer = PKCS1_v1_5.new(priv_key)
     digest = SHA256.new()
     digest.update(message)
@@ -75,52 +74,65 @@ def output():
     db.commit()
     time = signature[2].strftime(TIME_FORMAT)
     text = ','.join([signature[0], signature[1], time])
-    return render_template('timestamp.file.html', hash=signature[0], text=text, time=time)
+    return redirect(url_for('timestamp.file', hash=signature[0], text=text, time=time))
 
-@app.route('/download', methods=['POST'])
-def download():
+@app.route('/file')
+def file():
+    hash = request.args['hash']
+    text = request.args['text']
+    time = request.args['time']
+    return render_template('timestamp.file.html', hash=hash, text=text, time=time)
+
+@app.route('/downloadCert', methods=['POST'])
+def downloadCert():
     text = request.form['data']
-    signature = "kriptoportal podpis"
     plaintext_bytes = text.encode('utf-8')
-    signature_bytes = signature.encode('utf-8')
     with open(public, "r") as myfile:
         pub_key = RSA.importKey(myfile.read())
-    with open(private, "r") as myfile:
-        prv_key = RSA.importKey(myfile.read())
     encrypted = encrypt(plaintext_bytes,  pub_key )
-    signed = sign(signature_bytes, prv_key, "SHA-512")
-    return Response(signed+b'\r\n'+encrypted , mimetype="text/plain", headers={"Content-Disposition":"attachment;filename=Certificate.tsr"})
+    return Response(encrypted , mimetype="text/plain", headers={"Content-Disposition":"attachment;filename=Certificate.tsr"})
 
 @app.route('/checking')
 def checking():
     return render_template('timestamp.checking.html')
 
+@app.route('/pubkey')
+def pubkey():
+    with open(public, "r") as myfile:
+        key = myfile.read()
+    return render_template('timestamp.publickey.html', key = key)
+
+@app.route('/downloadKey', methods=['POST'])
+def downloadKey():
+    with open(public, "r") as myfile:
+        key = myfile.read()
+    return Response(key, mimetype="text/plain", headers={"Content-Disposition": "attachment;filename=PublicKey.cert"})
+
 @app.route('/check_file', methods=['POST'])
-def check():
+def check_file():
     file = returnFile()
     read = file.read()
-    msg = read.split(b'\r\n')
-    signature = "kriptoportal podpis"
-    with open(public, "r") as myfile:
-        pub_key = RSA.importKey(myfile.read())
-    verifyM = verify(signature.encode('utf-8'), msg[0], pub_key)
-    if (verifyM):
-        try:
-            with open(private, "r") as myfile:
-                key = RSA.importKey(myfile.read())
-            decrypted = decrypt(msg[1], key).decode('utf-8')
-            items = decrypted.split(",")
-            db = database.dbcon()
-            cur = db.cursor()
-            n = cur.execute("SELECT date, hashFile FROM timestamps WHERE hashPortal = %s AND hashFile = %s AND date = %s", (items[1], items[0], datetime.strptime(items[2], TIME_FORMAT)))
-            cur.close()
-            if n == 0:
-                return render_template('timestamp.file.html',
-                                       error="Z danim certifikatom ni bil potrjen noben dokument na tej strani.")
-            else:
-                return render_template('timestamp.file.html', check="Dokument z zgoščevalno funkcijo " + items[0] + " je bil potrjen " + items[2])
-        except:
-            pass
+    try:
+        with open(private, "r") as myfile:
+            key = RSA.importKey(myfile.read())
+        decrypted = decrypt(read, key).decode('utf-8')
+        items = decrypted.split(",")
+        db = database.dbcon()
+        cur = db.cursor()
+        #n = cur.execute("SELECT date, hashFile FROM timestamps WHERE hashPortal = %s AND hashFile = %s AND date = %s", (items[1], items[0], datetime.strptime(items[2], TIME_FORMAT)))
+        n = cur.execute("SELECT date, hashFile FROM timestamps WHERE hashPortal = %s AND hashFile = %s", (items[1], items[0]))
+        cur.close()
+        if n == 0:
+            return render_template('timestamp.file.html',
+                                   error="Z danim certifikatom ni bil potrjen noben dokument na tej strani.")
+        else:
+            return render_template('timestamp.file.html', check="Dokument z zgoščevalno funkcijo " + items[0] + " je bil potrjen " + items[2])
+    except:
+        pass
     return render_template('timestamp.file.html',
                            error="Prišlo je do napake pri preverjanju certifikata ali pa je certifikat neveljaven, poskusite ponovno.")
 
+@app.route('/check_hash', methods=['POST'])
+def check_hash():
+    fileHash = getHashFile()
+    return fileHash
