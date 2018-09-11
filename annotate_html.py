64009e72bd7e0fa0d6cd91c3500a33aff270c11e
filicmia <filicmia@gmail.com>
2018-09-12 00:49:47 +0200

    html localization

--- annotate_html.py
+++ annotate_html.py
@@ -1,12 +1,74 @@
+from bs4 import BeautifulSoup,Comment, NavigableString
+import re
 
-class Annotate(object):
-    """
-    Annotating html text for translating.
-    1. <h1>File Not Found</h1> -- ><h1>{{ _('File Not Found') }}</h1>
-    2. <p>New User? <a href="{{ url_for('register') }}">Click to Register!</a></p> -->
-       <p>{{ _('New User?') }} <a href="{{ url_for('register') }}">{{ _('Click to Register!') }}</a></p>
-    3. {{ user.followers.count() }} followers -- > {{ _('%(count)d followers', count=user.followers.count()) }}
+BEGIN = "{{_(\""
+END = "\")}}"
 
-    """ 
-    def parseHTML(html):
-        pass
+html = """<p class="description" dir="ltr">Name is a fine man.<br>"""+BEGIN+"""Name is cool</p>"""
+rootdir = "templates/"
+filename = "password.meter.html"
+
+soup = BeautifulSoup(open(rootdir+filename),"lxml")
+#comments=soup.find_all(string=lambda text: isinstance(text,Comment))
+
+for node in soup.find_all(text=lambda x: x.rstrip()): 
+    if node.parent.name == 'script' or isinstance(node, Comment):
+        continue
+    if not node:
+        continue
+    if node.startswith(BEGIN):
+        continue
+    if node.__contains__('{#'):
+        print(node)
+        continue
+
+    # If you need to use the regex more than once it is suggested to compile it.
+    pattern = re.compile(r"{%.*%}")
+    result = pattern.sub("", node)
+
+    node_new = node
+    if result != node:#
+        #print(node)
+        if not result.rstrip():#if there is only this (tetx in {% ... %}) and
+                                # nothing alse, do not do anything.
+            continue
+        else:
+            this = node.replace("{%", BEGIN+"{%")
+            this = this.replace("%}", "%}"+END)
+            node_new = NavigableString(this)
+            #node.replace_with(("{}").format(node_new))
+            print("bootstrap code ie else", node)
+
+    if node_new.startswith("{{"):
+        node_new = node_new.replace("{{", "")
+
+    if node_new.endswith("}}"):
+        this = node_new.replace("}}", "")
+        this = "\'%(content)s\', content="+this
+        node_new = NavigableString(this)
+    print(type(node))
+    node.replace_with(("{{{{_(\"{}\")}}}}").format(node_new))
+#print (soup.prettify())
+
+""" for text in soup.find_all(text=True):
+    #https://stackoverflow.com/questions/35472835/how-does-one-get-the-text-from-html-while-ignoring-formatting-tags-using-beautif
+    #ako nije već sourr
+    rstrip_text = text.rstrip()
+    if not rstrip_text:
+        continue
+    if text.rstrip().startswith(BEGIN):
+        continue
+    # If you need to use the regex more than once it is suggested to compile it.
+    pattern = re.compile(r"{%.*%}")
+
+    result = pattern.sub("", text)
+    if result != text:
+        if not result.rstrip():#if there is only this (tetx in {% ... %}) and
+                                # nothing alse, do not do anything.
+            continue
+    text.replace_with(BEGIN+text+END)
+ """
+
+filename = 'dev/'+filename
+with open(filename, "w") as file:
+    file.write(soup.prettify())
--- doc/localization.md
+++ doc/localization.md
@@ -1,2 +1,36 @@
-# Adding new translation
-1.
+# Adding new locale (link: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xiii-i18n-and-l10n)
+1. Run pybabel init -i messages.pot -d translations -l LANG
+(where LANG is changed with the short of the locale to be added)
+2. Make the translation in translations/LANG/LC_MESSAGES/messages.mo file
+3. pybabel compile -d translations
+ 
+
+# Adding any contect to the portal that will (eventualy) be shown to the user
+1. If the content is static .html, surround it with {{_(" ... ")}} .
+EXAMPLE1: 
+<input class="form-control" autocomplete="off" data-val-length-max="60" data-val-length-min="1" data-val-required="Vpisati morate geslo" name="pass" id="pass" placeholder="Vpišite geslo" rows="12" type="text" />
+
+change to
+
+<input class="form-control" autocomplete="off" data-val-length-max="60" data-val-length-min="1" data-val-required="{{_("Vpisati morate geslo")}}" name="pass" id="pass" placeholder="{{_("Vpišite geslo")}}" rows="12" type="text" />
+
+EXAMPLE2:
+<li><a href="{{url_for('timestamp.index')}}">Kaj je časovni žig?</a></li>
+
+change to
+
+<li><a href="{{url_for('timestamp.index')}}">{{_("Kaj je časovni žig?")}}</a></li>
+
+EXAMPLE3:
+
+<h1>Hi, {{current_user.username}}</h1>
+
+change to 
+
+<h1>{{ _('Hi, %(username)s!', username=current_user.username) }}</h1>
+
+
+2. Run: pybabel extract -F babel.cfg -k _l -o messages.pot .
+3. Run: pybabel update -i messages.pot -d translations
+4. Make the translation changes/additions in translations/LANG/LC_MESSAGES/messages.mo file
+5. Run: pybabel compile -d translations
