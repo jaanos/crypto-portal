1e047ce0b588a94568f58317fdf151dbbabaf301
Mia Filić <mfilic@Mias-MBP.lkrv>
2018-10-09 14:57:58 +0200

    mac req

---
+++ auth.py
@@ -0,0 +1,16 @@
+# Copy to auth.py
+# This sample data should be enough to test the parts of the site
+# which do not require a database connection
+
+dbase = 'database'
+dbuser = 'username'
+dbpass = 'password'
+dbhost = 'localhost'
+
+timestamp_public = "cert.pem"
+timestamp_private = "key.pem"
+
+# For Python 3, gitkey must be bytes
+gitkey = b'github-secret'
+sesskey = 'session-secret'
+debug = True
